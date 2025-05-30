f923898b5a10ba21feeee486516ba454cf98ac8c
yoyo8989 <kozinyou0850@gmail.com>
2025-05-31 00:14:12 +0900

    chore: trigger CI

--- scripts/world_sim.gd
+++ scripts/world_sim.gd
@@ -2,6 +2,6 @@ extends Node
 class_name WorldSim
 
 func simulate_years(years: int) -> void:
-    # --- ここに“大陸シミュレーション”のロジックを書く ---
-    # 例: for i in range(years): _simulate_one_year(i)
-    pass
+	# --- ここに“大陸シミュレーション”のロジックを書く ---
+	# 例: for i in range(years): _simulate_one_year(i)
+	pass
